65ab19d0e8d0009d52feb5ac893eac1d3b94e222
adil ghaffar <adilghaffar64@gmail.com>
2020-05-24 01:11:53 +0300

    first coomit

---
+++ leaderboard.py
@@ -0,0 +1,8 @@
+class LeaderboardItem:
+    def __init__(self, user_id, user_name, user_score):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.user_score = user_score
+
+class Leadboard:
+    
